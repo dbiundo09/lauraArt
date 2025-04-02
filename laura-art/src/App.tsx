2e169ec4a9aad138ead989553ee533c148b3c23a
Daniel Biundo <danielbiundo@Daniels-MacBook-Air.local>
2025-04-02 12:19:17 -0400

    m

---
+++ laura-art/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Laura's Art Gallery</h1>
+    </div>
+  );
+}
+
+export default App;
